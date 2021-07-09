34d8f28e1e64ee182e8ee09fb9a9886d666f6bb6
John Doe <john.doe@example.com>
2021-07-09 00:40:48 -0400

    Working with colors.

--- config.h
+++ config.h
@@ -21,7 +21,7 @@ static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#770000";
+static char selbordercolor[]        = "#eeeeee";
 static char selbgcolor[]            = "#005577";
 static char *colors[][3] = {
        /*               fg           bg           border   */
