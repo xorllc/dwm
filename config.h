d13aac98ace09618232eb443b7ba096efe03c2e5
John Doe <john.doe@example.com>
2021-11-04 23:38:40 -0400

    Trying https://github.com/honza/base16-st/blob/master/build/base16-brushtrees-dark-theme.h

--- config.h
+++ config.h
@@ -22,7 +22,7 @@ static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
 static char selbordercolor[]        = "#eeeeee";
-static char selbgcolor[]            = "#005577";
+static char selbgcolor[]            = "#4e694d";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
