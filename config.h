58ab4bab1b328245cc2bae4de9d8f625b1e99406
John Doe <john.doe@example.com>
2022-08-04 22:41:06 -0400

    Make the status bar a reddish color.

--- config.h
+++ config.h
@@ -23,7 +23,7 @@ static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
 static char selbordercolor[]        = "#eeeeee";
-static char selbgcolor[]            = "#59413e";
+static char selbgcolor[]            = "#6b3831";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
